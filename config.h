5220643c497b0b52018934368cf01f118fd99114
Carsten Cordes <carstencordes@gmx.de>
2019-05-24 16:12:15 +0200

    Changed from siji to fontawesome and included pywal colors

--- config.h
+++ config.h
@@ -1,3 +1,5 @@
+#include </home/badagent/.cache/wal/colors-wal-st.h>
+
 /* See LICENSE file for copyright and license details. */
 
 /*
@@ -6,8 +8,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font = "Fira Code:style=regular:size=10";
-static char *font2 = "Siji:style=regular";
+static char *font = "Fira Code:style=regular:antialias=true:size=12";
+static char *font2 = "Font Awesome 5 Free:style=regular";
 //static char *font = "Inconsolata:style=regular:size=10";
 //static char *font = "Source Code Pro:style=regular:size=10";
 static int borderpx = 2;
@@ -90,44 +92,45 @@ unsigned int tabspaces = 8;
 float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+//static const char *colorname[] = {
+//	/* 8 normal colors */
+//	"black",
+//	"red3",
+//	"green3",
+//	"yellow3",
+//	"blue2",
+//	"magenta3",
+//	"cyan3",
+//	"gray90",
 
-	[255] = 0,
+//	/* 8 bright colors */
+//	"gray50",
+//	"red",
+//	"green",
+//	"yellow",
+//	"#5c5cff",
+//	"magenta",
+//	"cyan",
+//	"white",
+
+//	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
-};
+//	"#cccccc",
+//	"#555555",
+//	"black",
+//};
 
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+//unsigned int defaultfg = 7;
+//unsigned int defaultbg = 258;
+//static unsigned int defaultcs = 256;
+//static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
